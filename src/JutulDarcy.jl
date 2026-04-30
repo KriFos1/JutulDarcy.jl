bc3fe879e3c3ea9e48d3cf31d60050095227578f
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-04-30 09:11:20 +0200

    Remove Tullio (#239)

--- src/JutulDarcy.jl
+++ src/JutulDarcy.jl
@@ -159,7 +159,7 @@ module JutulDarcy
     # PVT
     using MultiComponentFlash
     using MAT
-    using Tullio, LoopVectorization, Polyester
+    using LoopVectorization, Polyester
     using TimerOutputs
     using PrecompileTools
     using Dates
