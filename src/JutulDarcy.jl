604723099b794eb4cc63562af88d22ca4deff164
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-06-15 10:28:53 +0200

    Enthalpy formulation (#243)
    * More flexible check for if system uses CNV/MB
    
    * Simplify CNV/MB setup check
    
    * Update utils
    
    * Add abstract compositional LV system
    
    * Add optional field 'enthalpy' to BC and start making BC source computation more generic
    
    * Add enthalpy
    
    * Add missing enthalpy argument to constructor
    
    * Clean up bc utils
    
    * Support single-component models
    
    * Add support for setting non-standard tolerances through standard API
    
    * Improve initial state setup for thermal
    
    * Add surface enthalpy to facility
    
    * Add cross-term for surface enthalpy + improve well injection enthalpy logic
    
    * Set surface enthalpy to facility for thermal models
    
    * Add support for compositional systems
    
    * Remove obsolete BTES cross term
    
    * Typos
    
    * Proper total enthalpy computation
    
    * Add surface enthalpy equation scaling
    
    * Remove stale export
    
    * Remove stale export
    
    * Support setting per-cell density in BCs
    
    * Fix surface enthalpy scaling
    
    * Add docstring
    
    * Update docstring
    
    * Update docstring and force vectorization/de-vectorization
    
    * Improve BC enthalpy handling
    
    * Update tests
    
    * Extend BC enthalpy support to all fluid systems
    
    * Update docstring
    
    * Add type to SurfaceTemperature/Enthalpy
    
    * Change system name

--- src/JutulDarcy.jl
+++ src/JutulDarcy.jl
@@ -240,7 +240,7 @@ module JutulDarcy
 
     # Geothermal
     include("Geothermal/Geothermal.jl")
-    import JutulDarcy.Geothermal: ClosedLoopSupplyToReturnMassCT, ClosedLoopSupplyToReturnEnergyCT, BTESWellGroutEnergyCT
+    import JutulDarcy.Geothermal: ClosedLoopSupplyToReturnMassCT, ClosedLoopSupplyToReturnEnergyCT
 
     # Discrete Fracture Models
     include("DFM/cross_terms.jl")
